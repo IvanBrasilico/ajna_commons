9ecc9ccf60d852a0c979ddfc31bf18507af88ae3
IvanBrasilico <brasilico.ivan@gmail.com>
2018-02-15 14:30:19 -0200

    modulo login

--- ajna_commons/flask/login.py
+++ ajna_commons/flask/login.py
@@ -1,30 +1,45 @@
 from urllib.parse import urljoin, urlparse
 
-from flask import abort, redirect, render_template, request, url_for
-from flask_login import (LoginManager, UserMixin, login_required, login_user,
-                         logout_user)
+from flask import request
+from flask_login import (LoginManager, UserMixin)
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
-from virasana.views import app
 
 login_manager = LoginManager()
-login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.session_protection = 'strong'
 
 
 class DBUser():
+    dbsession = None
+
     def __init__(self, id):
         self.id = id
         self.name = str(id)
 
     @classmethod
-    def get(cls, session, username, password):
-        if username:
-            return DBUser(username)
+    def get(cls, username, password=None):
+        print('getting user', cls.dbsession)
+        if cls.dbsession:
+            if password:
+                print({'username': username,
+                       'password': password})
+                user = cls.dbsession.users.find_one({'username': username,
+                                                     'password': password})
+            else:
+                user = cls.dbsession.users.find_one({'username': username})
+            print('User retrieved ', user)
+            if user:
+                return DBUser(username)
+        else:
+            if username:
+                return DBUser(username)
         return None
+
+
+# db.users.find({'username': 'ivan', 'password': 'ivan'})
 
 
 class User(UserMixin):
@@ -36,8 +51,7 @@ class User(UserMixin):
 
     @classmethod
     def get(cls, username, password=None):
-        dbsession = ''
-        dbuser = cls.user_database.get(dbsession, username, password)
+        dbuser = cls.user_database.get(username, password)
         if dbuser:
             print('Nome:', dbuser.name)
             return User(dbuser.name)
@@ -68,33 +82,3 @@ def is_safe_url(target):
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ('http', 'https') and \
         ref_url.netloc == test_url.netloc
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('senha')
-        registered_user = authenticate(username, password)
-        if registered_user is not None:
-            print('Logged in..')
-            print(login_user(registered_user))
-            # print('Current user ', current_user)
-            next = request.args.get('next')
-            if not is_safe_url(next):
-                return abort(400)
-            return redirect(next or url_for('index'))
-        else:
-            return abort(401)
-    else:
-        return render_template('login.html', form=request.form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    next = request.args.get('next')
-    if not is_safe_url(next):
-        next = None
-    return redirect(next or url_for('index'))
---
+++ ajna_commons/flask/mongodbtest.py
@@ -0,0 +1,18 @@
+import os
+from pymongo import MongoClient
+
+
+MONGODB_URI = os.environ.get('MONGODB_URI')
+if MONGODB_URI:
+    DATABASE = ''.join(MONGODB_URI.rsplit('/')[-1:])
+    print(DATABASE)
+else:
+    DATABASE = 'test'
+
+db = MongoClient(host=MONGODB_URI)[DATABASE]
+
+
+user = db.users.find_one({'username': 'ivan', 'password': 'ivan'})
+
+if user is None:
+    db.users.insert({'username': 'ivan', 'password': 'ivan'})

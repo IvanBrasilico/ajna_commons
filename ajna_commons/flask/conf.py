53e7b7bb1ab4c0f6e6bae14994d575381edef7b0
IvanBrasilico <brasilico.ivan@gmail.com>
2018-02-17 10:57:17 -0200

    tox

--- ajna_commons/flask/conf.py
+++ ajna_commons/flask/conf.py
@@ -1,6 +1,10 @@
 import os
 import pickle
 import tempfile
+# TODO: Configure customized CSRF error
+from flask import render_template
+# from flask_wtf.csrf import CSRFError
+
 
 tmpdir = tempfile.mkdtemp()
 
@@ -20,18 +24,16 @@ if not SECRET:
         pickle.dump(SECRET, out, pickle.HIGHEST_PROTOCOL)
 
 
-#TODO: Configure customized CSRF error 
+# TODO: Configure customized CSRF error
 #  http://flask.pocoo.org/docs/0.12/patterns/packages/
-from flask import render_template
-from flask_wtf.csrf import CSRFError
+# @app.errorhandler(CSRFError)
 
-#@app.errorhandler(CSRFError)
+
 def handle_csrf_error(e):
     return render_template('csrf_error.html', reason=e.description), 400
 
-#TODO: Configure customized login error 
+# TODO: Configure customized login error
 #  http://flask.pocoo.org/docs/0.12/patterns/packages/
 # https://flask-login.readthedocs.io/en/latest/ - Customizing the login
-#login_manager.login_view = "users.login"
-#login_manager.login_message = u"Bonvolu ensaluti por uzi tiun paĝon."
-
+# login_manager.login_view = "users.login"
+# login_manager.login_message = u"Bonvolu ensaluti por uzi tiun paĝon."
